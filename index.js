ed31ca787073052e41c98de1f4bb6bdadce43118
Simón Negrín <simon.negrin@gmail.com>
2025-07-09 16:00:12 +0100

    Init project

---
+++ index.js
@@ -0,0 +1,113 @@
+import { WebSocketServer } from "ws"
+
+const port = 6100
+const rooms = new Map()
+const socketRooms = new WeakMap()
+const wss = new WebSocketServer({
+  port,
+})
+
+const actions = {
+  createroom,
+  joinroom,
+  broadcast,
+}
+
+wss.on('listening', () => {
+  console.log(`Server listening on port ${port}`)
+})
+
+wss.on("connection", (ws) => {
+  socketRooms.set(ws, new Set())
+
+  ws.on("message", (json) => {
+    try {
+      const message = JSON.parse(json)
+
+      if (!Object.hasOwn(actions, message.type)) {
+        console.warn("Unknown message type received:", message.type)
+        return
+      }
+
+      actions[message.type](ws, message)
+    } catch (error) {
+      console.error(error)
+    }
+  })
+
+  ws.on("close", () => {
+    const leaveRooms = socketRooms.get(ws)
+    leaveRooms.forEach(room => {
+      sendFrom(ws, room, { type: 'peerclose' })
+    })
+  })
+})
+
+function createroom(ws, message) {
+  const { roomId } = message
+
+  if (!roomId) {
+    throw new Error("Error creating room: Room ID is required to create room")
+  }
+
+  if (rooms.has(roomId)) {
+    throw new Error("Error creating room: Room ID already exists")
+  }
+
+  const room = {
+    connections: new Set([ws]),
+  }
+
+  socketRooms.get(ws).add(room)
+  rooms.set(roomId, room)
+  log(`Room created: ${roomId}`)
+}
+
+function joinroom(ws, message) {
+  const { roomId } = message
+
+  if (!roomId) {
+    throw new Error("Error joining room: Room ID is required to join room")
+  }
+
+  if (!rooms.has(roomId)) {
+    throw new Error("Error joining room: Room ID does not exists")
+  }
+
+  const room = rooms.get(roomId)
+  
+  room.connections.add(ws)
+  socketRooms.get(ws).add(room)
+
+  sendFrom(ws, room, { type: 'peerjoin' })
+  log(`Peer joined to room: ${roomId}`)
+}
+
+function broadcast(ws, message) {
+  const { roomId } = message
+  if (!roomId) {
+    throw new Error("Error broadcasting: Room ID is required to broadcast")
+  }
+
+  if (!rooms.has(roomId)) {
+    throw new Error("Error broadcasting: Room ID does not exists")
+  }
+
+  const room = rooms.get(roomId)
+  sendFrom(ws, room, message.data)
+  log(`Broadcast to room ${roomId}`)
+}
+
+function sendFrom(from, room, message) {
+  const encoded = JSON.stringify(message)
+  room.connections.forEach(ws => {
+    if (ws !== from) {
+      ws.send(encoded)
+    }
+  })
+}
+
+function log(...args) {
+  const time = new Date().toISOString()
+  console.log(`[${time}]`, ...args)
+}
